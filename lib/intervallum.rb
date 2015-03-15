a244eda65c420d9198112fd9e363e83e53b690d8
John Akers <john.akers@wildflowerhealth.com>
2015-03-15 15:52:47 -0700

    updated list of tested

--- lib/intervallum.rb
+++ lib/intervallum.rb
@@ -5,11 +5,11 @@ List of methods and examples as if today was March 2 2015
 - today                        # "2015-03-02"
 - this_day                     # "02"
 - wordy_day                    # "March 2, 2015"
-tomorrow                     # "2015-03-03", alias: next_day
-yesterday                    # "2015-03-01", alias: previous_day
+- tomorrow                     # "2015-03-03", alias: next_day
+- yesterday                    # "2015-03-01", alias: previous_day
 - this_month                   # "3"
-first_of_the_month           # "2015-03-01"
-last_month                   # "2015-02-01", alias: previous_month
+- first_of_the_month           # "2015-03-01"
+- last_month                   # "2015-02-01", alias: previous_month
 next_month                   # "2015-04-01"
 - wordy_month(3)             # "March"     , note:  argument can be string or integer
 last_year                    # "2014"      , alias: previous_year
